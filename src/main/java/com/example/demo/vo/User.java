eb817fa2eb03255540a5a57c265fa428fa128225
dongjun <294551397@qq.com>
2018-08-06 16:14:58 +0800

    add lombok

--- src/main/java/com/example/demo/vo/User.java
+++ src/main/java/com/example/demo/vo/User.java
@@ -1,5 +1,9 @@
 package com.example.demo.vo;
 
+
+import lombok.Data;
+
+@Data
 public class User {
     private String userid;
     private String username;
@@ -13,83 +17,4 @@ public class User {
 
     private String setid;
 
-    public String getUserid() {
-        return userid;
-    }
-
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(String createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(String updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public String getLastLoginTime() {
-        return lastLoginTime;
-    }
-
-    public void setLastLoginTime(String lastLoginTime) {
-        this.lastLoginTime = lastLoginTime;
-    }
-
-    public int getEnable() {
-        return enable;
-    }
-
-    public void setEnable(int enable) {
-        this.enable = enable;
-    }
-
-    public int getIsDisplay() {
-        return isDisplay;
-    }
-
-    public void setIsDisplay(int isDisplay) {
-        this.isDisplay = isDisplay;
-    }
-
-    public String getSourceType() {
-        return sourceType;
-    }
-
-    public void setSourceType(String sourceType) {
-        this.sourceType = sourceType;
-    }
-
-    public String getSetid() {
-        return setid;
-    }
-
-    public void setSetid(String setid) {
-        this.setid = setid;
-    }
 }
