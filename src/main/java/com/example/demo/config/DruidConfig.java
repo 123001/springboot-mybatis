2c91d2b8878807f10a67568865605048b934ab33
dongjun <294551397@qq.com>
2018-08-07 15:55:23 +0800

    update properties

--- src/main/java/com/example/demo/config/DruidConfig.java
+++ src/main/java/com/example/demo/config/DruidConfig.java
@@ -1,15 +1,12 @@
 package com.example.demo.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,11 +14,11 @@ import java.util.Map;
 @Configuration
 public class DruidConfig {
 
-    @Bean
+   /* @Bean
     @ConfigurationProperties(value = "spring.datasource")
     public DataSource druidDataSource() {
         return new DruidDataSource();
-    }
+    }*/
 
     @Bean
     public ServletRegistrationBean statViewServlet() {
--- src/test/java/com/example/demo/service/CountryServiceTest.java
+++ src/test/java/com/example/demo/service/CountryServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 
@@ -20,9 +21,14 @@ public class CountryServiceTest {
     @Autowired
     private CountryService countryService;
 
+
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     public void getAll() {
         List<Country> all = countryService.getAll(new Country());
         System.out.println("================"+all);
+        System.out.println("============"+dataSource);
     }
 }
